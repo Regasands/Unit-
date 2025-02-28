581b657b800f152f724491599a0245d36b73e338
Alexeykuz <alexeykuzmichenko@gmail.com>
2025-02-28 20:12:48 +0300

    animations on + button fix

--- app.py
+++ app.py
@@ -1,24 +1,20 @@
-from abc import ABCMeta
 import logging
-from math import log
-from os import stat_result
-import os
-import re
+
 import pygame
+
 from enemy.stone import Stone
 from game.state_game import Game, State
 from map.field import Field, FieldMenu, FieldShop, FieldEnd
 from map.texture import Button, Mob
 
-
 if __name__ == '__main__':
     logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s [%(levelname)s]  %(lineno)d %(message)s",
-    handlers=[
-        logging.FileHandler("app.log", encoding='utf-8'),
-        logging.StreamHandler()
-    ]
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s]  %(lineno)d %(message)s",
+        handlers=[
+            logging.FileHandler("app.log", encoding='utf-8'),
+            logging.StreamHandler()
+        ]
     )
     logging.info("Приложение запущено")
 
@@ -26,6 +22,7 @@ if __name__ == '__main__':
     pygame.mixer.music.load('music.mp3')  # Поддерживает MP3, WAV, OGG
     pygame.mixer.music.play(loops=-1, start=0.0)
     size: tuple = 800, 800
+    cell_size = size[0] // 10
     screen = pygame.display.set_mode(size)
     # создание основных полей
     field = Field()
@@ -36,10 +33,10 @@ if __name__ == '__main__':
     state_engine = State()
 
     background = pygame.image.load("sprites/background.png")
-    background = pygame.transform.scale(background, (800, 800))
+    background = pygame.transform.scale(background, size)
 
     end_background = pygame.image.load("sprites/end.png")
-    end_background = pygame.transform.scale(end_background, (800, 800))
+    end_background = pygame.transform.scale(end_background, size)
 
     while state_engine.start_game:
         if not state_engine.end:
@@ -64,31 +61,31 @@ if __name__ == '__main__':
                                 if struct == "error":
                                     continue
                                 if isinstance(struct, Button):
-                                        state_engine.game = False
-                                        if struct.name == 'Menu':
-                                                state_engine.menu = True
-                                        elif struct.name == 'Buy':
-                                                click = game.get_effect('click_mob')
-                                                for i in range(click):
-                                                    x, y = game.field_game.get_index_objects(None)
-                                                    level_mob = game.get_effect('level_upgrade')
-                                                    x_factor = game.x_hard / game.get_effect('x')
-                                                    mob = Mob('grass', 0, level_mob)
-                                                    logging.info(f'{level_mob}, {x_factor}')
-                                                    if x == 'full':
-                                                            game.set_alert(50, 'Full field')
-                                                    elif game.money >= mob.cost * x_factor:
-                                                            game.money -= mob.cost * x_factor
-                                                            game.money = int(game.money)
-                                                            game.field_game.field[y][x] = mob
-                                                    else:
-                                                            game.set_alert(50, 'Need more money')
-                                                state_engine.game = True
-                                        elif struct.name == 'Upgrade':
-                                                state_engine.shop = True
-                                        continue
+                                    state_engine.game = False
+                                    if struct.name == 'Menu':
+                                        state_engine.menu = True
+                                    elif struct.name == 'Buy':
+                                        click = game.get_effect('click_mob')
+                                        for i in range(click):
+                                            x, y = game.field_game.get_index_objects(None)
+                                            level_mob = game.get_effect('level_upgrade')
+                                            x_factor = game.x_hard / game.get_effect('x')
+                                            mob = Mob('grass', 0, level_mob)
+                                            logging.info(f'{level_mob}, {x_factor}')
+                                            if x == 'full':
+                                                game.set_alert(50, 'Full field')
+                                            elif game.money >= mob.cost * x_factor:
+                                                game.money -= mob.cost * x_factor
+                                                game.money = int(game.money)
+                                                game.field_game.field[y][x] = mob
+                                            else:
+                                                game.set_alert(50, 'Need more money')
+                                        state_engine.game = True
+                                    elif struct.name == 'Upgrade':
+                                        state_engine.shop = True
+                                    continue
                                 if isinstance(struct, Stone):
-                                        continue
+                                    continue
                                 x, y = field.get_coords_by_mouse_pos(event.pos)
                                 field.set_structure(None, (x, y))
                                 field.set_moving_structure(struct, event.pos)
@@ -112,7 +109,7 @@ if __name__ == '__main__':
                         structure = field_shop.get_structure_by_mouse_pos(event.pos)
                         if not isinstance(structure, Button):
                             break
-                        
+
                         if structure.name == 'Menu':
                             state_engine.menu = True
                             state_engine.shop = False
@@ -121,33 +118,33 @@ if __name__ == '__main__':
                             state_engine.shop = False
                         elif structure.name == 'BuyParam':
                             logging.info('Пошло обновление')
-                            game.complite_upgrade_updates() 
+                            game.complite_upgrade_updates()
                         elif structure.name == 'NextParam':
                             game.field_shop.update_last_scroll(True)
                         elif structure.name == 'BackParam':
                             game.field_shop.update_last_scroll(False)
-        # МЕНЮ
+            # МЕНЮ
             elif state_engine.menu:
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     object_ = field_menu.get_structure_by_mouse_pos(event.pos)
                     for sprite in field_menu.get_structure_sprites():
                         if sprite.rect.collidepoint(event.pos):
-                            object_ = field_menu.get_structure_by_mouse_pos(event.pos)
-                            if isinstance(object_, Button):
-                                    if object_.name == 'Start':
-                                            state_engine.menu = False
-                                            state_engine.game =True
-                                            game.x_hard = 1
-                                    elif object_.name == 'HardLevel':
-                                           game.x_hard = 1.5
-                                           state_engine.menu = False
-                                           state_engine.game = True
-
-                                    elif object_.name == 'DeletUpgrade':
-                                           game.reset_simple()
-                                           game.set_alert(10, 'Empty')
-                                           state_engine.menu = False
-                                           state_engine.game = True
+                            # Start
+                            if tuple(sprite.rect) == (cell_size * 4, cell_size * 4, cell_size * 2, cell_size * 2):
+                                state_engine.menu = False
+                                state_engine.game = True
+                                game.x_hard = 1
+                            # HardLevel
+                            elif tuple(sprite.rect) == (cell_size * 4, cell_size * 2, cell_size * 2, cell_size * 2):
+                                game.x_hard = 1.5
+                                state_engine.menu = False
+                                state_engine.game = True
+                            # DeletUpgrade
+                            elif tuple(sprite.rect) == (cell_size * 8, cell_size * 0, cell_size * 2, cell_size * 2):
+                                game.reset_simple()
+                                game.set_alert(30, 'Empty')
+                                state_engine.menu = False
+                                state_engine.game = True
             elif state_engine.end:
                 screen.blit(end_background, (0, 0))
                 if event.type == pygame.MOUSEBUTTONDOWN:
@@ -156,12 +153,12 @@ if __name__ == '__main__':
                         state_engine.menu = True
                         state_engine.end = False
                         field = Field()
-                        
 
                         field_menu = FieldMenu()
                         field_shop = FieldShop()
                         field_end = FieldEnd()
-                        game = Game(screen, field_menu=field_menu, field_shop=field_shop, field_game=field, field_end=field_end)
+                        game = Game(screen, field_menu=field_menu, field_shop=field_shop, field_game=field,
+                                    field_end=field_end)
                         state_engine = State()
 
         if state_engine.game:
@@ -171,18 +168,17 @@ if __name__ == '__main__':
             game.field_game.render_structures(screen)
 
         elif state_engine.shop:
-                game.field_shop.render_structures(screen)
-                game.render_update_inforamtion()
-                game.render_text_price()
-            # game.field_game.render_animations(screen)
+            game.field_shop.render_structures(screen)
+            game.render_update_inforamtion()
+            game.render_text_price()
         elif state_engine.end:
-                game.render_text_price(arg1 = 0, arg2 = 0)
-                game.set_alert(1000, 'You win')
-                game.field_end.render_structures(screen)
+            game.render_text_price(arg1=0, arg2=0)
+            game.set_alert(1000, 'You win')
+            game.field_end.render_structures(screen)
         elif state_engine.menu:
-                game.field_menu.render_structures(screen)
+            game.field_menu.render_structures(screen)
+        game.field_game.render_animations(screen)
         game.render_text_alert()
         pygame.display.flip()
         game.clock.tick(100)
 pygame.quit()
-
